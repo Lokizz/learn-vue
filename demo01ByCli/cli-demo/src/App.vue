01c64307e6f1a4d4b4ec9de80428a8ad4ec41429
MacLoki <newbeeloki@gmail.com>
2022-02-22 22:21:59 +0800

    添加了样式绑定和事件处理的案例

---
+++ demo01ByCli/cli-demo/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <!-- day2 数据绑定与遍历 -->
+    <!-- <data-binding></data-binding> -->
+    <!-- day3 事件与模板语法 -->
+    <evt-and-tpl></evt-and-tpl> 
+  </div>
+</template>
+
+<script>
+// import dataBinding from './components/day2_dataBinding.vue'
+import evtAndTpl from './components/day3_evtAndTpl.vue'
+// ! 默认通过这个语句来暴露模块
+export default {
+  components: {
+    // dataBinding,
+    evtAndTpl,
+  },
+  // 定义页面数据
+  data() {
+    // return {
+    //   msg: 'Hello world.',
+    //   user: {
+    //     name: 'John',
+    //     age: '23',
+    //   },
+    //   imgSrc: 'https://via.placeholder.com/200',
+    //   myHref: 'href',
+    //   myLink: 'http://www.baidu.com',
+    //   myList: ['a', 'b', 'c', 'd', 'e'],
+    //   myObj: {
+    //     name: 'loki',
+    //     age: 30,
+    //     sex: 'male'
+    //   }
+    // }
+  }
+}
+</script>
+
+<style>
+  .container {
+    width: 90vw;
+    margin: 0 auto;
+  }
+
+  .container > h2:not(h2:first-child) {
+    border-top: 1px solid lavender;
+    margin-top: 2rem;
+    padding-top: 2rem;
+  }
+
+  h2 {
+    color: lightcoral;
+    text-align: center;
+  }
+
+  h2 ~ h3 {
+    color: red;
+    text-align: center;
+  }
+</style>
---
+++ demo01ByCli/cli-demo/src/components/day2_dataBinding.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <h2>绑定数据</h2>
+    <p>{{ msg }}</p>
+    <p>绑定对象：<br>{{ user.name }} -- {{ user.age }}</p>
+
+    <h2>绑定属性</h2>
+    <h3>普通属性绑定</h3>
+    <img :src="imgSrc" alt="via.placeholder.com">
+    <h3>动态属性绑定</h3>
+    <!-- 动态属性值为静态数据时需要添加一对引号 -->
+    <a :[myHref]="'http://www.baidu.com'">跳转到百度</a>
+
+    <h2>遍历</h2>
+    <h3>遍历数组</h3>
+    <h3>v-for 在 Vue3 规定必须带有 :key 属性绑定，且可以嵌套遍历。</h3>
+    <ul>
+      <li v-for="(item, idx) in myList" :key="idx">Item{{ item }}</li>
+    </ul>
+    <h3>遍历对象</h3>
+    <ul>
+      <li v-for="(val, key) in myObj" :key="key">{{ val }}: {{ key }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default({
+  data(){
+    return {
+      msg: 'Hello world.',
+      user: {
+        name: 'John',
+        age: '23',
+      },
+      imgSrc: 'https://via.placeholder.com/200',
+      myHref: 'href',
+      myLink: 'http://www.baidu.com',
+      myList: ['a', 'b', 'c', 'd', 'e'],
+      myObj: {
+        name: 'loki',
+        age: 30,
+        sex: 'male'
+      }
+    }
+  }
+})
+</script>
+
+<style src="../style.css">
+</style>
